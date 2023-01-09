1b851770c6fd2d369f7c9696627d91872dd2010f
Linus Norton <linusnorton@gmail.com>
2023-01-09 14:06:53 +0000

    Add getter

--- src/main/java/uk/gov/hmcts/demo/ccd/model/Complaint.java
+++ src/main/java/uk/gov/hmcts/demo/ccd/model/Complaint.java
@@ -1,12 +1,16 @@
 package uk.gov.hmcts.demo.ccd.model;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
 import uk.gov.hmcts.ccd.sdk.api.CCD;
 
 import java.time.LocalDateTime;
 
 import static uk.gov.hmcts.ccd.sdk.type.FieldType.TextArea;
 
+@Getter
+@AllArgsConstructor
 public class Complaint {
 
     @CCD(typeOverride = TextArea)
