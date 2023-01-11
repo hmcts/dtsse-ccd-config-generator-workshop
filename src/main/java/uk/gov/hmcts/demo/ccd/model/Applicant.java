3c76edc1f048df529f86d99911ed7e217e3c531a
Linus Norton <linusnorton@gmail.com>
2023-01-11 07:21:45 +0000

    Add address field

--- src/main/java/uk/gov/hmcts/demo/ccd/model/Applicant.java
+++ src/main/java/uk/gov/hmcts/demo/ccd/model/Applicant.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import uk.gov.hmcts.ccd.sdk.type.AddressUK;
 
 @Data
 @AllArgsConstructor
@@ -13,5 +14,7 @@ public class Applicant {
     private String firstName;
 
     private String lastName;
+
+    private AddressUK address;
 
 }
